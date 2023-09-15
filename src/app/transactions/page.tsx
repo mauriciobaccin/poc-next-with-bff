64554473711e4dc87a1ab43008cf5eef75c6ed3d
Mauricio J Baccin <mauricio.baccin@remessaonline.com.br>
2023-09-15 18:16:06 -0300

    fix typo

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
 
           <h1>Lista de transactions SS:</h1>
 
-          <p>Get acontece no CLIENT side e renderiza a listagem sem um loader para o usuario</p>
+          <p>Get acontece no CLIENT side e renderiza a listagem com um loader para o usuario</p>
 
           <ListTransactionsCS />
 
